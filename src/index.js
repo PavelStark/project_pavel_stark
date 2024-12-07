1b336ead47a2f6191d4407406727278fc5b5fa1f
PavelStark <132998912+PavelStark@users.noreply.github.com>
2024-12-07 15:03:18 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ import { Provider } from "react-redux";
 import { persistor, store } from "./store";
 import { PersistGate } from "redux-persist/integration/react";
 
-export const ROOT_URL = "http://localhost:3333";
-/* export const ROOT_URL = "https://backend-s3ji.onrender.com"; */
+/* export const ROOT_URL = "http://localhost:3333";  */
+ export const ROOT_URL = "https://backend-s3ji.onrender.com"; 
 /* export const ROOT_URL = "https://backend-diplom-project-1.onrender.com"; */
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
